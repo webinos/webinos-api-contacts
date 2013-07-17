b9a747d86ba42828070d839169ab985fe11be8ce
John Lyle <john.lyle@cs.ox.ac.uk>
2013-07-17 10:04:52 +0100

    Add travis testing for the Contacts API
    This commit adds a set of scripts for automatically running the contacts
    tests through Travis CI.
    
    It adds the dev dependencies Jasmine and Zombie.
    
    Jira issue: WP-811

---
+++ test/tools/travis-test.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+################################################################################
+#  Code contributed to the webinos project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2013 John Lyle, University of Oxford
+################################################################################
+
+# Fail if anything fails
+set -e
+
+# Get and set some directories
+export API_NAME=webinos-api-contacts
+export API_DIR=`pwd`
+export TOOLS_DIR=$API_DIR/test/tools/
+export TMP=~/tmp
+export WEBINOS_PZP_DIR=$TMP/webinos-pzp
+
+# Assume we've already been installed by travis, but lets do it anyway
+npm install
+echo "Installed the Contacts api" 
+
+# Link this module
+npm link
+echo "Linked the Contacts api" 
+
+# move to a temporary directory 
+mkdir -p $TMP
+cd $TMP
+echo "Made directory $TMP " 
+
+# Empty it, and check out the PZP
+if [ -d "$WEBINOS_PZP_DIR" ]; then
+  rm -rf $WEBINOS_PZP_DIR
+  echo "Deleted directory $WEBINOS_PZP_DIR"
+fi
+git clone https://github.com/webinos/webinos-pzp.git $WEBINOS_PZP_DIR
+cd $WEBINOS_PZP_DIR
+
+# install the PZP and save dependencies
+npm install --save-dev
+echo "Installed the PZP" 
+
+#link to the API
+npm link $API_NAME
+echo "Linked $API_NAME " 
+
+#Start the PZP, grab its PID and wait 5 seconds
+node ./webinos_pzp.js &
+export PZP_PID=$!
+echo "Found PID: $PZP_PID "
+sleep 5
+echo "Started the PZP and waited" 
+
+# run the node test script and wait 2 seconds
+node $TOOLS_DIR/zombie-test.js
+sleep 2
+
+# kill the PZP
+kill -9 $PZP_PID
